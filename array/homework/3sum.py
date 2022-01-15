14896e8ca3c82d98f73e584cf64a9508699a9bf7
Seungsoo <seungsoo0701@gmail.com>
2022-01-15 14:18:46 +0900

    commit reverse_string

--- array/homework/3sum.py
+++ array/homework/3sum.py
@@ -12,9 +12,10 @@ class Solution:
         # 가장 작은 값을 고정하고, 중간값과 큰 값을 투 포인터로 계산한다. (하나의 값을 고정하고 그 뒤부터 투 포인터로 좁혀가며 계산한다)
         # 고정값 index = start.
         for start in range(len(nums) - 2):
+
             # 한 번 고정값으로 사용했다면 건너뛴다.
             if start > 0 and nums[start] == nums[start - 1]:
-                continue
+                continue #하단의 코드를 실행하지 않고 넘어감 ,즉 같은 고정값이라면 패스한다.
 
             # 투 포인터로 간격을 줄여가며 합 계산
             left, right = start + 1, len(nums) - 1
@@ -27,16 +28,24 @@ class Solution:
                     right -= 1
                 else:
                     answer.append([nums[start], nums[left], nums[right]])
-
                     # 중복연산 제거를 위해 left, right값 추가이동
-                    # 같은 것이 두번 들어갈 수도 있으므로 left나 right가
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
+
+                    # 같은 것이 두 번 들어갈 수도 있으므로 left나 right가
+                    # while left < right and nums[left] == nums[left + 1]:
+                    #     left += 1
+                    # while left < right and nums[right] == nums[right - 1]:
+                    #     right -= 1
+
                     left += 1
                     right -= 1
-        return print(answer)
+
+        for lists in answer:
+            lists = sorted(lists)
+
+        result = list(map(list,(set(map(tuple, answer)))))
+
+
+        return print(result)
 
 
 
