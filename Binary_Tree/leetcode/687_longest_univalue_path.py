2aeb89adf54730ff2e7556dcb09af5d5f5a20798
Seungsoo <seungsoo0701@gmail.com>
2022-01-26 00:09:52 +0900

    Update 687_longest_univalue_path.py

--- Binary_Tree/leetcode/687_longest_univalue_path.py
+++ Binary_Tree/leetcode/687_longest_univalue_path.py
@@ -7,6 +7,34 @@
 
 
 """
+from idlelib.tree import TreeNode
 
 
-def 
+class Solution:
+    result : int = 0
+
+    def longstUnivalue(self, root : TreeNode )->int:
+        def dfs(node : TreeNode):
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+
+            if node.right.val ==node.val:
+                right +=1
+            else:
+                right = 0
+
+            self.result = max(self.result, left+right)
+
+            return max(left,right)
+
+
+        dfs(root)
+        return self.result
