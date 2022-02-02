f4acb4cc0ba5610c341e0f7f7c8ff90a5e519970
Seungsoo <seungsoo0701@gmail.com>
2022-02-02 22:27:20 +0900

    특정 거리 도시 찾기 done

--- DFS/thiscodingTest/1_31.py
+++ DFS/thiscodingTest/1_31.py
@@ -2,64 +2,56 @@
 힙이 같은 부분 집합
 
 """
-#6
-#1 3 5 6 7 10
+# 6
+# 1 3 5 6 7 10
 
-N = 6 #int(input())
+N = 6  # int(input())
 
-nums = [1,3,5,6,7,10] # list(map(int, input().split()))
+nums = [1, 3, 5, 6, 7, 10]  # list(map(int, input().split()))
 total = sum(nums)
 
-res = "NO"
 
 def DFS(level: int, sub_sum: int, idx: int):
-    global res
-
-    if level == N or total/2 <= sub_sum:
-        return
-
-    if res == "YES":
-        return res
-
-    if sub_sum*2 == total:
-        res = "YES"
-        return
-
-    for i in range(idx, N):
-        return DFS(level + 1, sub_sum + nums[i], i + 1)
-
-
-DFS(0,0,0)
-
-if res == "YES":
-    print("YES")
-else:
-    print("NO")
-
-
-
-#############################################
-cnt = int(input())
-numbers = list(map(map(int, input().split())))
-accum = sum(numbers)
-flag = False
-
-def dfs(L, res):
-    global flag
-
-    if flag == True:
-        return True
-
-    if L ==cnt or res > accum // 2:
-        if accum - res == res:
-            flag = True
-            return True
+    if level == N or total / 2 <= sub_sum:
         return False
 
-    return dfs(L+1, res+numbers[L])+dfs(L+1, res)
+    if sub_sum * 2 == total:
+        return True
 
-result = dfs(0, 0)
-if result:
+    for i in range(N):
+        for j in range(i + idx, N):
+            return DFS(level + 1, sub_sum + nums[i], i + 1)
+
+
+res = DFS(0, 0, 0)
+
+if res:
     print("YES")
 else:
     print("NO")
+
+#############################################
+# cnt = int(input())
+# numbers = list(map(map(int, input().split())))
+# accum = sum(numbers)
+# flag = False
+#
+# def dfs(L, res):
+#     global flag
+#
+#     if flag == True:
+#         return True
+#
+#     if L ==cnt or res > accum // 2:
+#         if accum - res == res:
+#             flag = True
+#             return True
+#         return False
+#
+#     return dfs(L+1, res+numbers[L])+dfs(L+1, res)
+#
+# result = dfs(0, 0)
+# if result:
+#     print("YES")
+# else:
+#     print("NO")
--- DFS/thisiscodingtest/미로탈출.py
+++ DFS/thisiscodingtest/미로탈출.py
@@ -1,33 +1,60 @@
 from collections import deque
 
-N, M = 5, 6  # map(int, input().split())
 
+#행, 열 입력 받기
+N, M = map(int, input().split())
+
+# 그래프 입력 받기 위한 리스트 생성
 graph = []
 
+
+# 입력 받기
 for _ in range(N):
     graph.append(list(map(int, input())))
 
+
+# 상하좌우 방향 리스트
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
-queue = deque()
-count = 1
 
+
+#BFS 함수 생성 파라미터는 초기 위치값
 def BFS(x, y):
-    global count
-    if x < 0 or x >= N or y < 0 or y >= M:
-        return False
+    # deque 설정
+    queue = deque()
 
-    if graph[x][y] == 0:
-        pass ##????
-
-    if graph[x][y] == 1:
-
-
+    # 초기값 넣어주기
     queue.append((x, y))
 
+
+    # queue가 존재한다면 아직 내려갈 자식이 있다는 것이다.
     while queue:
+        # 부모노드 를 pop하여
         x, y = queue.popleft()
 
+        # 상하좌우로 움직인다.
         for i in range(4):
-            BFS(x + dx[i], y + dy[i])
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            #움직일때 범위 밖이면 그냥 continue로 넘어간다.
+            if nx < 0 or ny < 0 or nx >= N or ny >= M:
+                continue
+
+            # 범위 안이지만 괴물 위치라면 넘어간다.
+            if graph[nx][ny] == 0:
+                continue
+
+            # 만약에 통로라면 부모노드가 가지고 있는 값에 +1을 하여 현재 갈 수 있는 통로에 넣어준다.
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+
+                # 현재 지나갈 수 있는 통로들!을 queue에 넣어줘서 다시 돌릴 수 있도록 하게 한다.
+                queue.append((nx, ny))
+
+    #함수가 다 끝난다면 배열이므로 전체의 마지막 자식을 꺼낸다면 최소한 이동 칸의 갯수를 val로 가지고 있을 것이므로 위를 return해주면 된다.
+    return graph[N - 1][M - 1]
+
+
+print(BFS(0, 0))
