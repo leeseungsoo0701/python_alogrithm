29578f39f66ba91d62ff3048303db78384debf87
Seungsoo <seungsoo0701@gmail.com>
2022-02-09 21:19:38 +0900

    commit files

--- greedy/thisiscodingtest/무지의 먹방 라이브.py
+++ greedy/thisiscodingtest/무지의 먹방 라이브.py
@@ -1,56 +1,13 @@
-K = int(input())
-food_times = list(map(int, input().split()))
+from collections import deque
 
-array = []
-
-
-def mukbang(food_times: list[int], K: int) -> int:
-    for idx, val in enumerate(food_times):
-        array.append([val, idx])
-
-    total = sum(food_times)
-
-    if total <= K:
-        return -1
-
-    count = 0
-    len_food_times = len(food_times)
-    min_food_times = min(food_times)
-    cycle = K // len_food_times
-    etc = K % len_food_times
-
-    if cycle <= min_food_times:
-        count += cycle * len_food_times
-        K -= count
-        for i in range(len_food_times):
-            array[i][0] = array[i][0] - cycle
-
-    else:
-        etc += (cycle - min_food_times) * len_food_times
-        cycle = min_food_times
-        count += cycle * len_food_times
-        K -= count
-        for j in range(len_food_times):
-            array[j][0] = array[j][0] - cycle
-
-    for k in range(etc):
-        if array[k][0] == 0:
-            continue
+def solution(food_times, k):
+    food_times = deque([[value, idx] for idx, value in enumerate(food_times)])
+    for _ in range(k):
+        food_times[0][0] -= 1
+        if food_times[0][0]:
+            food_times.rotate(-1)
         else:
-            array.append(array[k][0] - 1)
-            count += 1
-
-    return array[count//len_food_times][1]
-
-
-print(mukbang(food_times, K))
-
-
-
-############################################################
-K = int(input())
-food_times = list(map(int, input().split()))
-
-array = []
-
-
+            food_times.popleft()
+            if not food_times:
+                return -1
+    return food_times[0][1] + 1
